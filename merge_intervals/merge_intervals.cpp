2c0ff6b734eadad73490b5c54ef3c1df37c5a7ed
ananyaannachhatre <ananya.annachhatre@gmail.com>
2025-09-17 09:50:40 +0530

    Added Merge Intervals Problem

---
+++ merge_intervals/merge_intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        sort(intervals.begin(), intervals.end());
+        int k=0;
+        int n = intervals.size();
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        for(int i=1; i<n; i++){
+            if(ans[k][1] >= intervals[i][0]){
+                ans[k][1] = max(ans[k][1], intervals[i][1]);
+            }
+            else{
+                ans.push_back(intervals[i]);
+                k++;
+            }
+        }
+        return ans;
+    }
+};
